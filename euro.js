7606278e4df3679270d5b1352b84093634acc66d
Don Marti <dmarti@zgp.org>
2018-05-21 11:34:33 -0700

    WIP: Check for "ga" and set Google Analytics options
    This does not seem to work yet -- "ga" is not present even
    though it shows up later.
    
    Will have to try another approach.
    
    Refs #3.

--- euro.js
+++ euro.js
@@ -1,12 +1,24 @@
 window.addEventListener('load', function() {
+	console.log("Content script euro.js starting.");
+
 	// put Aloodo into "European mode".  (For pre-#GDPR testing.)
-	if ( window.eval('aloodo') && (window.eval('typeof(aloodo.setEuropeanMode)') == 'function')) {
+	if ( window.hasOwnProperty('aloodo')) {
 		window.eval('aloodo.setEuropeanMode(true)');
 	}
 	
-	if (window.eval('googletag')) {
+	if (window.hasOwnProperty('googletag')) {
 		console.log("googletag present.  Attempting to set non-personalized ads.");
 		window.eval('googletag.pubads().setRequestNonPersonalizedAds(1)');
 	}
+
+	if (window.hasOwnProperty('ga')) {
+		console.log("Google Analytics present. Setting anonymizeIp to true and disabling Advertising Reporting Features.");
+		window.eval("window.ga('set', 'anonymizeIp', true)");
+		window.eval("window.ga('set', 'displayFeaturesTask', null)");
+	} else {
+		console.log('no window.ga present');
+	}
+	
+	console.log("Content script euro.js ending.");
 });
 
