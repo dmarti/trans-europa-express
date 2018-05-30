f898346d5a485ddbefb114395288d3d2002d619d
Don Marti <dmarti@zgp.org>
2018-05-30 13:39:22 -0700

    Keep track of which settings have already been applied

--- euro.js
+++ euro.js
@@ -1,35 +1,39 @@
 (function(){
-	// Try 20 times/s for 3 s
+	// Try 20 times/s for 5 s
 	var retryTimeout = 50;
-	var attempts = 60;
+	var attempts = 100;
+	var europeanStuffDone = {};
 	console.log("Content script euro.js starting.");
 
 	function insistOnEuropeanitude(attempts) {
 		var wW = window.wrappedJSObject;
 
 		// put Aloodo into "European mode". 
-		if ('aloodo' in wW) {
+		if ('aloodo' in wW && !europeanStuffDone.hasOwnProperty('aloodo')) {
 			wW.aloodo.setEuropeanMode(true);
 			console.log("Aloodo in European mode");
+			europeanStuffDone['aloodo'] = true;
 		}
-	
-		if ('googletag' in wW) {
-			console.log("googletag present.  Attempting to set non-personalized ads.");
+
+		// Google Tag Manager
+		if ('googletag' in wW && !europeanStuffDone.hasOwnProperty('googletag')) {
 			wW.googletag.pubads().setRequestNonPersonalizedAds(1);
+			console.log("googletag present.  Set non-personalized ads.");
+			europeanStuffDone['googletag'] = true;
 		} 
 
-		if ('ga' in wW) {
+		// Google Analytics
+		if ('ga' in wW && !europeanStuffDone.hasOwnProperty('ga')) {
 			wW.ga('set', 'anonymizeIp', true);
 			wW.ga('set', 'displayFeaturesTask', null);
 			console.log("ga options set");
+			europeanStuffDone['ga'] = true;
 		} 
 		
 		if (attempts) {
 			setTimeout(insistOnEuropeanitude, retryTimeout, attempts -1);
-			return;
 		}
 	}
 	insistOnEuropeanitude(attempts - 1);
-	console.log("Content script euro.js ending.");
 })();
 
