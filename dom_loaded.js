b117d87935bf13ab7e3ebee4269d0f36e1c8b5ba
Don Marti <dmarti@zgp.org>
2018-06-01 14:40:30 -0700

    Be more verbose to avoid an error in strict mode
    What is it with ad scripts and console warnings?

--- dom_loaded.js
+++ dom_loaded.js
@@ -1,7 +1,7 @@
 (function(){
 	var wD = document.wrappedJSObject;
 	var abg = wD.createElement('script');
-	abg.innerHTML = "(adsbygoogle=window.adsbygoogle||[]).requestNonPersonalizedAds=1";
+	abg.innerHTML = "if window.hasOwnProperty('adsbygoogle') { window.adsbygoogle.requestNonPersonalizedAds=1; } else { var adsbygoogle = []; adsbygoogle.requestNonPersonalizedAds=1;}";
 	wD.documentElement.appendChild(abg);
 })();
 
