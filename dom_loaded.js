6e32d3c6f20b190a1c8fd21e4e9631a0f9d13bb7
Don Marti <dmarti@zgp.org>
2018-06-04 09:57:19 -0700

    Fix error in injected adsbygoogle script

--- dom_loaded.js
+++ dom_loaded.js
@@ -1,7 +1,8 @@
 (function(){
 	var wD = document.wrappedJSObject;
 	var abg = wD.createElement('script');
-	abg.innerHTML = "if window.hasOwnProperty('adsbygoogle') { window.adsbygoogle.requestNonPersonalizedAds=1; } else { var adsbygoogle = []; adsbygoogle.requestNonPersonalizedAds=1;}";
+	abg.innerHTML = "if (window.hasOwnProperty('adsbygoogle') && window.adsbygoogle) { window.adsbygoogle.requestNonPersonalizedAds=1; } else { var adsbygoogle = []; adsbygoogle.requestNonPersonalizedAds=1;}";
 	wD.documentElement.appendChild(abg);
+	wD.documentElement.removeChild(abg);
 })();
 
