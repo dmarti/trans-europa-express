2b7ae47f5a770f1302bb58030f596e163af64fa0
Don Marti <dmarti@zgp.org>
2019-11-23 06:51:05 -0800

    Set "allow_ad_personalization_signals" to false.
    If Google Tag Manager or Google Analytics are on the
    page, set allow_ad_personalization_signals to false.
    
    This is based on Google documentation at:
    https://support.google.com/google-ads/answer/9606827
    
    Fixes #6

--- euro.js
+++ euro.js
@@ -2,7 +2,15 @@
 	var retryTimeout = 50;
 	var attempts = 100;
 	var europeanStuffDone = {};
-	console.log("Content script euro.js starting.");
+
+	function doDataLayer() {
+		var wD = document.wrappedJSObject;
+		var tmp = wD.createElement('script');
+		tmp.innerHTML = "window.dataLayer = window.dataLayer || []; " +
+				"window.dataLayer.push({'allow_ad_personalization_signals': false});"
+		wD.documentElement.appendChild(tmp);
+		wD.documentElement.removeChild(tmp);
+	}
 
 	function insistOnEuropeanitude(attempts) {
 		var wW = window.wrappedJSObject;
@@ -16,6 +24,7 @@
 
 		// Google Tag Manager
 		if ('googletag' in wW && !europeanStuffDone.hasOwnProperty('googletag')) {
+			doDataLayer();
 			wW.googletag.pubads().setRequestNonPersonalizedAds(1);
 			console.log("googletag present.  Set non-personalized ads.");
 			europeanStuffDone['googletag'] = true;
@@ -23,6 +32,7 @@
 
 		// Google Analytics
 		if ('ga' in wW && !europeanStuffDone.hasOwnProperty('ga')) {
+			doDataLayer();
 			wW.ga('set', 'anonymizeIp', true);
 			wW.ga('set', 'displayFeaturesTask', null);
 			console.log("ga options set");
@@ -33,6 +43,7 @@
 			setTimeout(insistOnEuropeanitude, retryTimeout, attempts -1);
 		}
 	}
+	console.log("Content script euro.js starting.");
 	insistOnEuropeanitude(attempts - 1);
 })();
 
